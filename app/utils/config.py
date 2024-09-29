390aec5f336d341a9ecd4aada6e893c1a70c81b8
Dong Heon Lee <Sirius506775@gmail.com>
2024-09-30 05:38:11 +0900

    [refactor] : Update callback url

--- app/utils/config.py
+++ app/utils/config.py
@@ -8,11 +8,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_SECRET_KEY = os.getenv("GOOGLE_SECRET_KEY")
-GOOGLE_CALLBACK_URI = "http://127.0.0.1:5500/google/callback.html"
+GOOGLE_CALLBACK_URI = "https://balpyo.site/google/callback"
 
 KAKAO_CLIENT_ID = os.getenv("KAKAO_CLIENT_ID")
 KAKAO_SECRET_KEY = os.getenv("KAKAO_SECRET_KEY")
-KAKAO_CALLBACK_URI = "http://127.0.0.1:5500/kakao/callback.html"
+KAKAO_CALLBACK_URI = "https://balpyo.site/kakao/callback"
 
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
